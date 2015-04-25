880fc330091c58d257bd23ea0eff6d4e57a6b698
Ramon Gebben <ramon@ra-ge.net>
2015-04-25 22:34:04 +0200

    Test hooks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var taskListing = require('gulp-task-listing');
+var shell = require('shelljs');
  
 // Add a task to render the output 
 gulp.task('help', taskListing);
@@ -12,6 +13,14 @@ gulp.task('api-server', function(){
 		env: { 'NODE_ENV': 'development' }
 	});
 });	
+gulp.task('check-in', function(){
+	if (!shell.which('git')) {
+	  echo('Sorry, this task requires git, please install.');
+	  exit(1);
+	}	
+	shell.cd(__dirname + '/databases');
+	shell.exec('rethinkdb dump');
+});
 
 gulp.task('api', ['api-server']);
 
