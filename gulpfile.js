930be7a5e158f615b31735c5768a5f10572e942f
Ralf Nieuwenhuijsen <r.nieuwenhuijsen@synovite.nl>
2015-04-25 03:41:36 +0200

    * added grunt task 'help' which lists all available grunt commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var taskListing = require('gulp-task-listing');
+ 
+// Add a task to render the output 
+gulp.task('help', taskListing);
 
 gulp.task('server', function(){
 	nodemon({
