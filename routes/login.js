1dbc39642a3f874f401147b2bef51d9f96379179
Ralf Nieuwenhuijsen <r.nieuwenhuijsen@synovite.nl>
2015-05-02 18:08:23 +0200

    * rewrote login logic to be more generic

--- routes/login.js
+++ routes/login.js
@@ -16,29 +16,29 @@ r.connect({
 module.exports = [
 {
     method: 'POST',
-    path: '/login/twitter',
+    path: '/login',
     config: {
         validate: {
             payload: {
-                id: Joi.number().required(),
-                accessToken: Joi.string(),
-                accessTokenSecret: Joi.string()
+                email: Joi.string(),
+                twitter_id: Joi.number(),
+                key: Joi.string().required()
             }
         },
         tags: ['accounts', 'api', 'post'],
         description: 'Login with a twitter id'
     },
     handler: function( reaccount, reply ){        
-        r.table('accounts').filter({twitter: {id: reaccount.payload.id}}).limit(1).run(connection, function(err, cursor) {
+        r.table('accounts').filter({auth: reaccount.payload }).limit(1).run(connection, function(err, cursor) {
             if (err) reply( err );
             cursor.toArray(function(err, result) {
                 if( result.length === 0 ){                    
-                    r.table('accounts').insert({twitter: reaccount.payload}).run(connection, function( err, result ){                        
+                    r.table('accounts').insert({auth: reaccount.payload}).run(connection, function( err, result ){                        
                         if( err ) reply( err );
-                        else reply( reaccount.payload );
+                        else reply( { id: result.generated_keys[0] } );
                     });
                 }            
-                else reply( result );    
+                else reply( { id: result[0].id } );    
             });
         });        
     }
