abce7e1dc67c6d18313ff3fb2fd46cbe887120fc
jjwedemyer <mail@jakob-wedemyer.de>
2017-05-09 13:41:49 +0200

    disabling questions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var quests = require('./quests');
-var quizes = require('./quizes');
 var questions = require('./questions');
 var accounts = require('./accounts');
 var meerkats = require('./meerkats');
 var login = require('./login.js');
 var apps = require('./apps.js');
 
-module.exports = [].concat(quests, accounts, meerkats, login, apps, quizes);
+module.exports = [].concat(quests, accounts, meerkats, login, apps);
